0e5f98736c45eb307a85a6791af9af621bf85bfc
apjgaming <lewiand1234@gmail.com>
2025-05-30 14:13:51 -0600

    w06 registration view

--- routes/accountRoute.js
+++ routes/accountRoute.js
@@ -6,6 +6,6 @@ const accountController = require("../controllers/accountController")
 
 // Route to deliver the "My Account" page
 router.get("/login", utilities.handleErrors(accountController.buildLogin))
-
+router.get("/register", utilities.handleErrors(accountController.buildRegister))
 // Export the router
 module.exports = router
