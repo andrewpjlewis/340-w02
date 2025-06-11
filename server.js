c846e6eeda183d69119c5c14d899b0a6e73e31f2
apjgaming <lewiand1234@gmail.com>
2025-06-11 15:30:20 -0600

    update for render production

--- server.js
+++ server.js
@@ -28,12 +28,7 @@ app.use(session({
   saveUninitialized: true
 }));
 
-const pool = new Pool({
-  connectionString: process.env.DATABASE_URL,
-  ssl: {
-    rejectUnauthorized: false
-  }
-});
+
 
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true })) // for parsing application/x-www-form-urlencoded
